69619dbb7dea8a67c3d1162086c8a4a4f740a210
VincentHuynh1016 <114731503+VincentHuynh1016@users.noreply.github.com>
2024-07-01 22:18:21 -0700

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_pymongo import PyMongo
+from bson import ObjectId
+from datetime import datetime
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
@@ -21,6 +23,8 @@ def submit_rating():
     amentities_rating = data.get('amenitiesRating', 0)
     comment = data.get('comment', "")
 
+    current_date = datetime.utcnow().strftime("%b %d, %Y")
+
     try:
         # Insert the ratings into MongoDB
         mongo.db.ratings.insert_one({
@@ -32,12 +36,28 @@ def submit_rating():
             "safety_ratings": safety_Ratings,
             "amenities_rating": amentities_rating,
             "comment": comment,
+            "date": current_date
 
         })
         return jsonify({"success": True, "message": "Ratings submitted successfully"}), 200
     except Exception as e:
         print(f"Error inserting data: {e}")
         return jsonify({"success": False, "message": "Database error"}), 500
+
+
+#Grabbing all the data so I can show it on the website
+@app.route('/getRatings', methods=['GET'])
+def get_ratings():
+    try:
+        ratings = list(mongo.db.ratings.find())
+        for rating in ratings:
+            rating['_id'] = str(rating['_id'])  # Convert ObjectId to string
+        return jsonify(ratings), 200
+    except Exception as e:
+        print(f"Error retrieving data: {e}")
+        return jsonify({"success": False, "message": "Database error"}), 500
+
+
 
 if __name__ == '__main__':
     app.run(debug=True, port = 8080)
